3274a5c0bbc1849a46ccb7860ca18b0ea62e785c
cdelannoy1 <139888905+cdelannoy1@users.noreply.github.com>
2023-10-22 16:51:18 +0200

    Update wholesome_customers.py

--- wholesome_customers.py
+++ wholesome_customers.py
@@ -1,3 +1,12 @@
-import streamlit as st 
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+import plotly.express as px
+import plotly.graph_objects as go
+
+df = df = pd.read_csv("wholesome_customers_data.csv")
+
 
 
