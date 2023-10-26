cb0a3a3c19ea5fe65df40a16bad29b19e84b5137
cdelannoy1 <139888905+cdelannoy1@users.noreply.github.com>
2023-10-26 19:11:39 +0200

    Update wholesome_customers.py

--- wholesome_customers.py
+++ wholesome_customers.py
@@ -40,10 +40,9 @@ def main():
 
 
  
-    elif choice=="Exploratory Data Analysis":
+     elif choice=="Exploratory Data Analysis":
 	    
 	st.write("Exploratory Data Analysis Section")
-
 	st.header('Exploratory Data Analysis')
 	st.markdown('## Python for Data Analysis 2')
 	st.markdown('#### Wholesome Customers Dataset Analysis') 
